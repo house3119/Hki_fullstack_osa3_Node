96a87531b9b1a3397b548c416baf1559d6a16cfe
Antti Varis <ajvari88@gmail.com>
2024-04-14 23:33:40 +0300

    teh3.3

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ app.get('/api/persons', (request, response) => {
 })
 
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if(!person) {
+        response.status(404).end()
+    } else {
+        response.json(person)
+    }
+})
+
+
 app.get('/info', (request, response) => {
     response.send(`
         <p>Phonebook has info for ${persons.length} people.</p>
