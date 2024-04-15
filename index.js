d4e62a0c5fe9ec8d3dac1cea768f45173374067d
Antti Varis <ajvari88@gmail.com>
2024-04-15 22:16:24 +0300

    teh3.4

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+
+let persons = [
     {
         "id": 1,
         "name": "Arto Hellis",
@@ -33,6 +34,13 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.json(person)
     }
+})
+
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
 })
 
 
