f61826b85864442f7db8e4902df26d5f13144e39
Antti Varis <ajvari88@gmail.com>
2024-04-23 22:58:26 +0300

    Välietenemistä

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-
+const Person = require('./models/person')
 const app = express()
-
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
+// Middleware setup
 const catcher = (req, res, next) => {
     if(req.body) {
         res.locals.holder = JSON.stringify(req.body)
@@ -29,27 +30,11 @@ const customMorgan = (tokens, req, res) => {
 app.use(morgan(customMorgan))
 
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellis",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-37645"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-049786"
-    },
-]
-
-
+// Routes
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 
@@ -105,3 +90,4 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
---
+++ models/person.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('Connecting to database...')
+mongoose.connect(url)
+
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+    id: Number,
+    name: String,
+    number: String
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+})
+const Person = mongoose.model('Person', personSchema)
+
+module.exports = mongoose.model('Person', personSchema)
