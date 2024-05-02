22c3976ad81d9754ae03dda42550f5298aa9771a
Antti Varis <ajvari88@gmail.com>
2024-05-02 23:38:52 +0300

    teh3.19

--- index.js
+++ index.js
@@ -120,7 +120,9 @@ app.get('/info', (request, response, next) => {
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     if (error.name === 'CastError') {
-      return response.status(400).send({ Error: 'Malformatted id' })
+        return response.status(400).send({error: 'Malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).send({error: error.message})
     }
     next(error)
 };
